ca1c48cbd68113e437559b93218166b8055717c0
Maina-M19 <maina1999m19@gmail.com>
2025-04-07 16:03:06 +0530

    Connected Backend with frontend

--- frontend/src/components/EditModal.jsx
+++ frontend/src/components/EditModal.jsx
@@ -14,12 +14,59 @@ import {
 	ModalOverlay,
 	Textarea,
 	useDisclosure,
+    useToast,
 } from "@chakra-ui/react";
+import { useState } from "react";
 import { BiEditAlt } from "react-icons/bi";
+import { BASE_URL } from "../App";
 
-function EditModal() {
+function EditModal({setUsers, user}) {
 	const { isOpen, onOpen, onClose } = useDisclosure();
+    const [isLoading, setIsLoading] = useState(false)
+    const toast = useToast()
 
+    const [inputs, setInputs] = useState({
+        name: user.name,
+        role: user.role,
+        description: user.description,
+    })
+
+    const handleEditUser = async (e) =>{
+        e.preventDefault()
+        setIsLoading(true)
+        try{
+            const res = await fetch(BASE_URL+"/friends/"+user.id,{
+                method: "PATCH",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(inputs)
+            })
+            const data = await res.json()
+            if(!res.ok){
+                throw new Error(data.error)
+            }
+            setUsers((prevUsers) => prevUsers.map((u) => u.id === user.id ? data : u))
+            toast({
+                status: "success",
+                title: "Yayy! 🎉",
+                description: "Friend updated successfully.",
+                duration: 2000,
+                position: "top-center",
+            })
+            onClose()
+        } catch(error){
+            toast({
+                status: "error",
+                title: "An error occurred.",
+                description: error.message,
+                duration: 400,
+                position: "top-center",
+            })
+        } finally {
+            setIsLoading(false)
+        }
+    }
 	return (
 		<>
 			<IconButton
@@ -33,6 +80,7 @@ function EditModal() {
 
 			<Modal isOpen={isOpen} onClose={onClose}>
 				<ModalOverlay />
+                <form onSubmit={handleEditUser}>
 				<ModalContent>
 					<ModalHeader>My new BFF 😍</ModalHeader>
 					<ModalCloseButton />
@@ -40,12 +88,16 @@ function EditModal() {
 						<Flex alignItems={"center"} gap={4}>
 							<FormControl>
 								<FormLabel>Full Name</FormLabel>
-								<Input placeholder='John Doe' />
+								<Input placeholder='John Doe'
+                                value={inputs.name} 
+                                onChange={(e)=>setInputs((prev) =>({...prev,name: e.target.value}))} />
 							</FormControl>
 
 							<FormControl>
 								<FormLabel>Role</FormLabel>
-								<Input placeholder='Software Engineer' />
+								<Input placeholder='Software Engineer' 
+                                value={inputs.role} 
+                                onChange={(e)=>setInputs((prev) =>({...prev,role: e.target.value}))} />
 							</FormControl>
 						</Flex>
 						<FormControl mt={4}>
@@ -53,19 +105,21 @@ function EditModal() {
 							<Textarea
 								resize={"none"}
 								overflowY={"hidden"}
-								placeholder="He's a software engineer who loves to code and build things.
-              "
+								placeholder="He's a software engineer who loves to code and build things."
+                                value={inputs.description} 
+                                onChange={(e)=>setInputs((prev) =>({...prev,description: e.target.value}))}      
 							/>
 						</FormControl>
 					</ModalBody>
 
 					<ModalFooter>
-						<Button colorScheme='blue' mr={3}>
-							Add
+						<Button colorScheme='blue' mr={3} type="submit">
+							Update
 						</Button>
 						<Button onClick={onClose}>Cancel</Button>
 					</ModalFooter>
 				</ModalContent>
+                </form>
 			</Modal>
 		</>
 	);
